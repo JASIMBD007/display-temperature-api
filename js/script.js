f341bf34d116d0c5405f7bce5e740e89db0159b4
Jasim Uddin <jasim.ice.iu@gmail.com>
2022-11-13 20:13:15 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const API_KEY = `cc7fa384097bafdadffafb84400c67ff`;
+const searchTemperature = () => {
+    const city = document.getElementById('city-name').value;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => displayTemperature(data))
+};
+
+const setInnerText = (id, text) => {
+    document.getElementById(id).innerText = text;
+}
+
+const displayTemperature = temperature => {
+    setInnerText('city', temperature.name);
+    setInnerText('temperature', temperature.main.temp);
+
+    console.log(temperature);
+
+}
